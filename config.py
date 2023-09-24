fa2fd9b1e6fe8fcfe5043fa0d6eb2ee1bc14f83e
Siddharthpandey0O <145746872+Siddharthpandey0O@users.noreply.github.com>
2023-09-24 10:59:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,11 +21,11 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5174492401").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itszshivam/SK_musicbot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Siddharthpandey0O/nityamusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_skshivam")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/over_poweredd")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/fun_here")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
